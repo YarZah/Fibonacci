9784849f98f9dde2337a9b6e2ad661a4880ca7a8
YarZah <jarikzagreb16@gmail.com>
2024-10-20 16:44:23 +0200

    Fibo 10

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int n = 3;
+        int n = 10;
         System.out.println("Число Фібоначчі для n = " + n + ": " + Fibo.fibon(n));
     }
 }
